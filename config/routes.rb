5b053e9cf33de8e4d5507aeaf0670cfa97323e67
dreamchaser3 <dreamchaser3@naver.com>
2017-05-05 09:11:45 +0000

    customize devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,  :controllers => {
+    :registrations => "users/registrations",
+    :sessions =>"users/sessions",
+    :passwords =>"users/passwords",
+    :confirmations =>"users/confirmations",}
   get 'home/index'
   root 'home#index'
   get 'home/friends' => 'home#friends'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505080003) do
+ActiveRecord::Schema.define(version: 20170505090343) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20170505080003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "background_img"
+    t.string   "profile_img"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
