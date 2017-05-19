2797e7203f9f5d15fe9f88c1c7ffd9cc2e279cdd
Julia Oh <letsgohyunju@gmail.com>
2017-05-19 10:20:30 +0000

    confirm

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20170509072937) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "img",                                 null: false
     t.string   "title"
     t.string   "content"
     t.integer  "category",                            null: false
@@ -26,11 +25,12 @@ ActiveRecord::Schema.define(version: 20170509072937) do
   end
 
   create_table "replies", force: :cascade do |t|
-    t.string   "content",    null: false
-    t.integer  "post_id",    null: false
-    t.string   "user_email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "content",                null: false
+    t.integer  "post_id",                null: false
+    t.string   "user_email",             null: false
+    t.integer  "status",     default: 1, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "user_friends", force: :cascade do |t|
@@ -42,21 +42,21 @@ ActiveRecord::Schema.define(version: 20170509072937) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                                                                                                                    null: false
+    t.string   "encrypted_password",     default: "",                                                                                                                    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                                                                                                                     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                                                                                                             null: false
+    t.datetime "updated_at",                                                                                                                                             null: false
     t.string   "username"
-    t.string   "background_img"
-    t.string   "profile_img"
+    t.string   "background_img",         default: "http://article.images.consumerreports.org/prod/content/dam/cro/news_articles/health/CR-Health-Crop-Food-Child-10-16"
+    t.string   "profile_img",            default: "https://help.github.com/assets/images/help/profile/identicon.png"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
